a6a412c2a0ba2994a8bfcbcf06a7d22d0f6189e6
Abel Kalum <chumbabel@gmail.com>
2020-03-13 20:12:10 +0000

    modify environment file

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,5 +1,12 @@
 require_relative "./boston_museums/version"
 require_relative "./boston_museums/cli"
+require_relative "./boston_museums/scraper"
+require_relative "./boston_museums/category"
+require_relative "./boston_museums/museums"
+
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 
 module BostonMuseums
   class Error < StandardError; end
