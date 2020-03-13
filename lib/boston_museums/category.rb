e10180e2a4b65d617ad22c908319961e793391d1
Abel Kalum <chumbabel@gmail.com>
2020-03-13 20:16:42 +0000

    add category file in lib folder

---
+++ lib/boston_museums/category.rb
@@ -0,0 +1,20 @@
+class BostonMuseums::Category
+  attr_accessor :name, :urL
+  attr_reader :museums
+  @@all = []
+ def initialize(name, url)
+  @name = name
+  @url = "https://www.tripadvisor.com/Attractions-g60745-Activities-c49-Boston_Massachusetts.html" + url
+  @museums = [] 
+  @@all << self
+ end
+
+ def self.all
+  @@all
+ end
+
+ def add_museum(museum)
+  self.museums << museum
+  museum.category = self
+ end
+end
