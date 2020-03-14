cf12fb2097fe9742ed00db014a145b88de4df946
Abel Kalum <chumbabel@gmail.com>
2020-03-14 03:18:50 +0000

    modify cli methods

--- lib/boston_museums/cli.rb
+++ lib/boston_museums/cli.rb
@@ -88,31 +88,26 @@ class BostonMuseums::CLI
   end
 
 def scrape_art_galleries
-  category == "Art Galleries"
-  category.url == "https://www.tripadvisor.com/Attractions-g60745-Activities-c49-t1-Boston_Massachusetts.html"
-  BostonMuseums::Scraper.scrape_art_galleries(category.url)
+  url = "https://www.tripadvisor.com/Attractions-g60745-Activities-c49-t1-Boston_Massachusetts.html"
+  BostonMuseums::Scraper.scrape_art_galleries(url)
 end
 
 def scrape_history_museums
-  category == "History Museums"
-  category.url == "https://www.tripadvisor.com/Attractions-g60745-Activities-c49-t30-Boston_Massachusetts.html" 
-  BostonMuseums::Scraper.scrape_history_museums(category.url)
+  url = "https://www.tripadvisor.com/Attractions-g60745-Activities-c49-t30-Boston_Massachusetts.html" 
+  BostonMuseums::Scraper.scrape_history_museums(url)
 end
 
 def scrape_specialty_museums
-  category == "Specialty Museums"
-  category.url == "https://www.tripadvisor.com/Attractions-g60745-Activities-c49-t161-Boston_Massachusetts.html" 
-  BostonMuseums::Scraper.scrape_specialty_museums(category.url)
+  url = "https://www.tripadvisor.com/Attractions-g60745-Activities-c49-t161-Boston_Massachusetts.html" 
+  BostonMuseums::Scraper.scrape_specialty_museums(url)
 end
 
 def scrape_art_museums
-  category == "Art Museums"
-  category.url == "https://www.tripadvisor.com/Attractions-g60745-Activities-c49-t28-Boston_Massachusetts.html"
-  BostonMuseums::Scraper.scrape_art_museums(category.url)
+  url = "https://www.tripadvisor.com/Attractions-g60745-Activities-c49-t28-Boston_Massachusetts.html"
+  BostonMuseums::Scraper.scrape_art_museums(url)
 end
 
 def scrape_science_museums
-  category == "Science Museums"
-  category.url == "https://www.tripadvisor.com/Attractions-g60745-Activities-c49-t35-Boston_Massachusetts.html" 
-  BostonMuseums::Scraper.scrape_science_museums(category.url)
+  url = "https://www.tripadvisor.com/Attractions-g60745-Activities-c49-t35-Boston_Massachusetts.html" 
+  BostonMuseums::Scraper.scrape_science_museums(url)
 end
