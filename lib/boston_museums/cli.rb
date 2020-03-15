fb3bde19d44477a6c23eb27996874c25434852e8
Abel Kalum <chumbabel@gmail.com>
2020-03-15 18:34:38 +0000

    modify cli methods

--- lib/boston_museums/cli.rb
+++ lib/boston_museums/cli.rb
@@ -7,8 +7,8 @@ class BostonMuseums::CLI
  
   def menu
    puts "What category of museums would you like to visit today?"
-   puts "Art Galleries, History Museums, Specialty Museums, Art Museums, or Science Museums"
-   puts "Type either '1' for Art Galleries, '2' for History Museums, '3' for Specialty Museums, '4' for Art Museums, or '5' for Science Museums"
+   puts "Art Galleries, History Museums, Specialty Museums, Art Museums, Science Museums, Children's Museums, Military Museums, or Observatories and Planetariums"
+   puts "Type either '1' for Art Galleries, '2' for History Museums, '3' for Specialty Museums, '4' for Art Museums, '5' for Science Museums, '6' for Children's Museums, '7' for Military Museums, or '8' for Observatories and Planetariums"
      input = gets.strip
      case input
        when "1"
@@ -22,25 +22,43 @@ class BostonMuseums::CLI
        if BostonMuseums::Category.all == []
          scrape_categories
        end
-         display_category_museums('History Museums')
+         display_category_museums("History Museums")
        when "3"
        puts "in Specialty Museums"
        if BostonMuseums::Category.all == []
          scrape_categories
        end
-         display_category_museums('Specialty Museums')
+         display_category_museums("Specialty Museums")
        when "4"
        puts "in Art Museums"
        if BostonMuseums::Category.all == []
          scrape_categories
        end
-         display_category_museums('Art Museums')
+         display_category_museums("Art Museums")
        when "5"
        puts "in Science Museums"
        if BostonMuseums::Category.all == []
          scrape_categories
        end
-         display_category_museums('Science Museums')
+         display_category_museums("Science Museums")
+       when "6"
+       puts "in Children's Museums"
+       if BostonMuseums::Category.all == []
+         scrape_categories
+       end
+         display_category_museums("Children's Museums")
+       when "7"
+       puts "in Military Museums"
+       if BostonMuseums::Category.all == []
+         scrape_categories
+       end
+         display_category_museums("Military Museums")
+       when "8"
+       puts "in Observatories and Planetariums"
+       if BostonMuseums::Category.all == []
+         scrape_categories
+       end
+         display_category_museums("Observatories and Planetariums")
        when "exit"
          puts "Goodbye!"
        else
@@ -60,7 +78,7 @@ class BostonMuseums::CLI
     end
       puts "Here are the museums in #{category}:\n"
       BostonMuseums::Category.all.each do |museum|
-        puts "#{museums}"
+        puts "#{museum.name}"
         puts "#{museum_reviews}"
         puts "#{museum_ratings}"
     end
--- lib/boston_museums/scraper.rb
+++ lib/boston_museums/scraper.rb
@@ -2,11 +2,12 @@ class BostonMuseums::Scraper
   def self.scrape_categories(category)
    url = "https://www.tripadvisor.com/"
    webpage = Nokogiri::HTML(open(url))
-   section = webpage.css("div.filter_list_1")
-   array_of_links = section.css("div.jfy_checkbox.ui_input_checkbox.multifilter.cx_brand_refresh_phase2")
-
+   section = webpage.css("div.attraction_list_all.easyClear")
+   array_of_links = section.css("div div.listing_details")
+   
    array_of_links.map do |link|
-  BostonMuseums::Category.new(link.text)
+     binding.pry
+   BostonMuseums::Category.new(link.text, link.attributes["href"].value)
 end
 
  def self.scrape_museums(category)
@@ -16,7 +17,7 @@ end
     museums.each do |card|
       museum = BostonMuseums::Museum.new
 
-      museums = card.css("div.listing_title.title_with_snippets h2").text.strip
+      museum.name = card.css("div.listing_title.title_with_snippets h2").text.strip
       museum.reviews = card.css("div.review_snippets").text.strip
       museum.ratings  = card.css("div.popRanking.wrap").text.strip
 
